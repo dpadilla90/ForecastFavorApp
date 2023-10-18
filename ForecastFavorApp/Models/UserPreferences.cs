a829d7495636c8535c0a958a0c80eba1b384c33c
Dennis Padilla <d_padilla.y@hotmail.com>
2023-10-17 23:28:03 -0400

    Added comments to the 'WeatherModel' and 'UserPreferences' classes.
     The comments explain what each class and field does in the ForecastFavorApp. We also described the 'AppTheme' and 'NotificationType' lists. This makes the code easier to read and understand. Now, anyone looking at the code will know what each part is for. This helps us work together better and make changes in the future

--- ForecastFavorApp/Models/UserPreferences.cs
+++ ForecastFavorApp/Models/UserPreferences.cs
@@ -6,29 +6,45 @@ using System.Threading.Tasks;
 
 namespace ForecastFavorApp.Models
 {
+    /// <summary>
+    /// Represents user-specific settings and preferences for the application.
+    /// 
+    /// Fields:
+    /// - Id: A unique identifier for each user's preferences.
+    /// - SavedLocations: List of locations the user is interested in tracking weather for.
+    /// - IsCelsius: Preference for displaying temperature in Celsius. True for Celsius, False for Fahrenheit.
+    /// - Theme: The preferred app theme (Light or Dark).
+    /// - NotificationPreferences: List of weather conditions the user wants to be notified about.
+    /// </summary>
     public class UserPreferences
     {
-        public Guid Id { get; set; } = Guid.NewGuid(); 
-
-       
+        public Guid Id { get; set; } = Guid.NewGuid();
         public List<string> SavedLocations { get; set; } = new List<string>();
-
         public bool IsCelsius { get; set; } = true;
-
-       
         public AppTheme Theme { get; set; } = AppTheme.Light;
-
-        
         public List<NotificationType> NotificationPreferences { get; set; } = new List<NotificationType>();
     }
 
+    /// <summary>
+    /// Enumerates the possible themes for the app's appearance.
+    /// </summary>
     public enum AppTheme
     {
         Light,
         Dark
-
     }
 
+    /// <summary>
+    /// Enumerates the types of weather notifications a user can opt to receive.
+    /// 
+    /// Values:
+    /// - RainyDay: Notification for rainy days.
+    /// - SunnyDay: Notification for sunny days.
+    /// - StormAlert: Notification for stormy weather.
+    /// - CloudyDay: Notification for cloudy days.
+    /// - WindyDay: Notification for windy days.
+    /// - SnowyDay: Notification for snowy days.
+    /// </summary>
     public enum NotificationType
     {
         RainyDay,
@@ -37,6 +53,6 @@ namespace ForecastFavorApp.Models
         CloudyDay,
         WindyDay,
         SnowyDay
-        
     }
 }
+
--- ForecastFavorApp/Models/WeatherModel.cs
+++ ForecastFavorApp/Models/WeatherModel.cs
@@ -6,6 +6,20 @@ using System.Threading.Tasks;
 
 namespace ForecastFavorApp.Models
 {
+    /// <summary>
+    /// Represents a comprehensive weather model for a specific location.
+    /// 
+    /// Fields:
+    /// - LocationName: The name of the location for which the weather details are provided.
+    /// - CurrentCondition: Describes the current weather condition (e.g., Rainy, Sunny).
+    /// - CurrentTemperature: The current temperature for the location.
+    /// - Humidity: The current humidity percentage.
+    /// - WindSpeed: The speed of the wind.
+    /// - ChanceOfPrecipitation: Percentage chance that precipitation will occur.
+    /// - Recommendation: Provides user-friendly advice based on the weather conditions.
+    /// - HourlyForecasts: List of hourly weather forecasts.
+    /// - ThreeDayForecasts: List of daily weather forecasts for the next three days.
+    /// </summary>
     public class WeatherModel
     {
        
@@ -21,11 +35,13 @@ namespace ForecastFavorApp.Models
 
      
         public List<HourlyForecast> HourlyForecasts { get; set; }
-
         
         public List<DailyForecast> ThreeDayForecasts { get; set; }
     }
 
+    /// <summary>
+    /// Represents the weather forecast for a specific hour.
+    /// </summary>
     public class HourlyForecast
     {
         public DateTime Time { get; set; }  
@@ -33,6 +49,17 @@ namespace ForecastFavorApp.Models
         public string Condition { get; set; }
     }
 
+    /// <summary>
+    /// Represents the weather forecast for a specific day.
+    /// 
+    /// Fields:
+    /// - Date: The date of the forecast.
+    /// - Condition: Describes the weather condition for the day.
+    /// - HighTemperature: The highest expected temperature for the day.
+    /// - LowTemperature: The lowest expected temperature for the day.
+    /// - ChanceOfPrecipitation: Percentage chance that precipitation will occur.
+    /// - Recommendation: Provides user-friendly advice based on the weather forecast.
+    /// </summary>
     public class DailyForecast
     {
         public DateTime Date { get; set; }
