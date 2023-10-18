6411ec9e721b3ab47221d6dd31c1cadc0c35bb93
Dennis Padilla <d_padilla.y@hotmail.com>
2023-10-17 23:16:06 -0400

    Added the UserPreferences model to ForecastFavorApp.
    This lets users save their favorite weather locations and choose temperature settings. Users can pick if they want temperatures in Celsius. They can also select between light or dark app themes. There's a list for weather notifications too. So, users can get alerts for weather like sunny, rainy, or stormy days. This makes the app user-friendly and personalized

---
+++ ForecastFavorApp/Models/UserPreferences.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForecastFavorApp.Models
+{
+    public class UserPreferences
+    {
+        public Guid Id { get; set; } = Guid.NewGuid(); 
+
+       
+        public List<string> SavedLocations { get; set; } = new List<string>();
+
+        public bool IsCelsius { get; set; } = true;
+
+       
+        public AppTheme Theme { get; set; } = AppTheme.Light;
+
+        
+        public List<NotificationType> NotificationPreferences { get; set; } = new List<NotificationType>();
+    }
+
+    public enum AppTheme
+    {
+        Light,
+        Dark
+
+    }
+
+    public enum NotificationType
+    {
+        RainyDay,
+        SunnyDay,
+        StormAlert,
+        CloudyDay,
+        WindyDay,
+        SnowyDay
+        
+    }
+}
