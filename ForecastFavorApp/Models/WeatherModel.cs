77a810bbda4b858c6ff1c07e334e58bc3b892d6a
Dennis Padilla <d_padilla.y@hotmail.com>
2023-10-17 23:04:57 -0400

    Integrated the WeatherModel to our ForecastFavor project.
    This model contains critical weather details such as the current temperature, humidity, weather conditions, wind speed, and chance of precipitation. In addition to presenting current data, it also provides a comprehensive breakdown of hourly forecasts and a 3-day forecast overview, ensuring users are well-prepared for the day and days ahead.
    
    The model additionally offers a unique Recommendation field, tailoring user experiences by suggesting activities aligned with the forecast. The incorporated sub-models, HourlyForecast and DailyForecast, further refine and categorize the forecast details for enhanced user readability and app functionality.

---
+++ ForecastFavorApp/Models/WeatherModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForecastFavor.Models
+{
+    public class WeatherModel
+    {
+       
+        public string LocationName { get; set; }
+
+      
+        public string CurrentCondition { get; set; }
+        public int CurrentTemperature { get; set; }
+        public int Humidity { get; set; }
+        public string WindSpeed { get; set; } 
+        public int ChanceOfPrecipitation { get; set; }
+        public string Recommendation { get; set; }
+
+     
+        public List<HourlyForecast> HourlyForecasts { get; set; }
+
+        
+        public List<DailyForecast> ThreeDayForecasts { get; set; }
+    }
+
+    public class HourlyForecast
+    {
+        public DateTime Time { get; set; }  
+        public int Temperature { get; set; }
+        public string Condition { get; set; }
+    }
+
+    public class DailyForecast
+    {
+        public DateTime Date { get; set; }
+        public string Condition { get; set; }
+        public int HighTemperature { get; set; }
+        public int LowTemperature { get; set; }
+        public int ChanceOfPrecipitation { get; set; }
+        public string Recommendation { get; set; }
+    }
+}
